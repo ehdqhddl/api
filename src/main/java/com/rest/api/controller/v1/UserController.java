bf8c8c41277194c01aa26ec4fcd31237436ee37d
DongBongE <55652438+ehdqhddl@users.noreply.github.com>
2020-04-01 00:22:43 +0900

    RestAPI 인터페이스 및 결과 데이터 구조 설계
    RestAPI 인터페이스 및 결과 데이터 구조 설계

--- src/main/java/com/rest/api/controller/v1/UserController.java
+++ src/main/java/com/rest/api/controller/v1/UserController.java
@@ -1,14 +1,16 @@
 package com.rest.api.controller.v1;
 
 import com.rest.api.entity.User;
+import com.rest.api.model.response.CommonResult;
+import com.rest.api.model.response.ListResult;
+import com.rest.api.model.response.SingleResult;
 import com.rest.api.repo.UserJpaRepo;
+import com.rest.api.service.ResponseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Api(tags = {"1. User"})
 @RequiredArgsConstructor
@@ -16,21 +18,53 @@ import java.util.List;
 @RequestMapping(value = "/v1")
 public class UserController {
     private final UserJpaRepo userJpaRepo;
+    private final ResponseService responseService; // 결과를 처리할 service
 
-    @ApiOperation(value ="회원 조회", notes = "모든 회원을 조회한다.")
-    @GetMapping(value = "/user")
-    public List<User> findAllUser(){
-        return userJpaRepo.findAll();
+    @ApiOperation(value ="회원 리스트 조회", notes = "모든 회원을 조회한다.")
+    @GetMapping(value = "/users")
+    public ListResult<User> findAllUser(){
+        // 결과 데이터가 여러건인경우 getListResult 를 이용해서 결과를 출력한다.
+        return responseService.getListResult(userJpaRepo.findAll());
+    }
+
+    @ApiOperation(value ="회원 단건 조회", notes = "User ID로 회원을 조회한다.")
+    @GetMapping(value = "/user/{msrl}")
+    public SingleResult findUserById(@ApiParam(value="회원ID", required = true)@PathVariable long msrl){
+        // 결과 데이터가 여러건인경우 getListResult 를 이용해서 결과를 출력한다.
+        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElse(null));
     }
 
     @ApiOperation(value = "신규 등록", notes = "신규회원을 등록한다.")
     @PostMapping(value = "/user")
-    public User save(@ApiParam(value = "회원 아이디", required = true) @RequestParam String uid,
-                     @ApiParam(value = "회원 이름", required = true) @RequestParam String name){
+    public SingleResult<User> save(@ApiParam(value = "회원 아이디", required = true) @RequestParam String uid,
+                                   @ApiParam(value = "회원 이름", required = true) @RequestParam String name){
         User user = User.builder()
                 .uid(uid)
                 .name(name)
                 .build();
-        return userJpaRepo.save(user);
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @ApiOperation(value = "회원 수정", notes = "회원정보를 수정한다.")
+    @PutMapping(value="/user")
+    public SingleResult<User> modify(
+            @ApiParam(value = "회원번호", required = true)@RequestParam long msrl,
+            @ApiParam(value = "회원아이디", required = true)@RequestParam String uid,
+            @ApiParam(value = "회원이름", required = true)@RequestParam String name){
+        User user = User.builder()
+                .msrl(msrl)
+                .uid(uid)
+                .name(name)
+                .build();
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @ApiOperation(value="회원 삭제", notes = "User ID로 회원정보를 삭제한다.")
+    @DeleteMapping(value="/user/{msrl}")
+    public CommonResult delete(
+            @ApiParam(value="회원번호",required = true) @PathVariable long msrl){
+        userJpaRepo.deleteById(msrl);
+        // 성공결과 정보만 필요한경우 getSuccessResult()를 이용하여 결과를 출력한다.
+        return responseService.getSuccessResult();
     }
 }
